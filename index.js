749e2d8a7ed376376d7b75b79a1a22680597aa61
Your Name <akshita.srivastava.1505@gmail.com>
2026-01-13 23:26:27 +0530

    ddn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { GoogleGenAI } from "@google/genai";
 import express from "express"
 
+import dotenv from "dotenv";
+dotenv.config();
 
 const app=express();
 app.use(express.json());
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({apiKey:"AIzaSyATBGge1qQhxRJECCffxQ-IS2r57UTj9Nc"});
+const ai = new GoogleGenAI({});
 
 app.post("/generate",async(req,res)=>{
     const {prompt}=req.body;
